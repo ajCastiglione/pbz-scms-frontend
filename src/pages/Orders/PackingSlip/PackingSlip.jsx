a0b1f4ec055106a3ab88e1ab2d90f18dbc263b3a
sarslan-work <82287184+sarslan-work@users.noreply.github.com>
2021-11-26 11:04:40 -0500

    update for qa issues

--- src/pages/Orders/PackingSlip/PackingSlip.jsx
+++ src/pages/Orders/PackingSlip/PackingSlip.jsx
@@ -1,73 +1,83 @@
 // React Imports
-import React, { useEffect, useState } from 'react';
-// Axios 
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+// Axios
+import axios from "axios";
 // React-router
-import { useRouteMatch, } from 'react-router';
+import { useRouteMatch } from "react-router";
 // Styles
-import classes from './PackingSlip.module.scss';
+import classes from "./PackingSlip.module.scss";
 
 const PackingSlip = () => {
-    const route = useRouteMatch();
-    const [data, setData] = useState();
-    const date = new Date()
+  const route = useRouteMatch();
+  const [data, setData] = useState();
+  const date = new Date();
 
-    useEffect(() => {
-        axios.get(`/order/packing-slip/${route.params.id}`)
-            .then(res => {
-                setData(res.data)
-                window.print()
-            })
-            .catch(err => {
-                window.alert(err.response.data.message)
-            })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(`/order/packing-slip/${route.params.id}`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        window.print();
+      })
+      .catch((err) => {
+        window.alert(err.response.data.message);
+      });
+  }, []);
 
-    
-
-    return (
-        <React.Fragment>
-            <div className={classes.Container}>
-            <h1>Packing Slip</h1>
-        
-        {data && data.order.customer_reference ? <h1>{ data.order.customer_reference }</h1> : null}
-
+  return (
+    <React.Fragment>
+      <div className={classes.Container}>
+        <h1>Packing Slip</h1>
+        {data && data.order.customer_reference ? (
+          <h1>{data.order.customer_reference}</h1>
+        ) : null}
         Ship To:
-
-        {data ? <pre>
-            { data.order.recipient.name }<br />
-            { data.order.recipient.contact }<br />
-            { data.order.recipient.street1 }<br />
-            { data.order.recipient.street2 }<br />
-            { data.order.recipient.city }, { data.order.recipient.state } { data.order.recipient.postal }<br />
-            { data.order.recipient.country }<br />
-            { data.order.recipient.phone }<br />
-        </pre> : null}
-
-        {data ? <table>
+        {data ? (
+          <pre>
+            {data.order.recipient?.name ?? ""}
+            <br />
+            {data.order.recipient?.contact ?? ""}
+            <br />
+            {data.order.recipient?.street1 ?? ""}
+            <br />
+            {data.order.recipient?.street2 ?? ""}
+            <br />
+            {data.order.recipient?.city ?? ""},
+            {data.order.recipient?.state ?? ""}{" "}
+            {data.order.recipient?.postal ?? ""}
+            <br />
+            {data.order.recipient?.country ?? ""}
+            <br />
+            {data.order.recipient?.phone ?? ""}
+            <br />
+          </pre>
+        ) : null}
+        {data ? (
+          <table>
             <tr>
-                <th>Item Name</th>
-                <th>Item #</th>
-                <th>Description</th>
-                <th># Cases</th>
-                <th># Units</th>
-                </tr>
-            {data.lineItem.map((line) => (
-            <tr key={line._id}>
-                <td>{ line.item.name }</td>
-                <td>{ line.item.number }</td>
-                <td>{ line.item.description }</td>
-                <td>{ line.quantity_cases }</td>
-                <td>{ line.quantity_units }</td>
+              <th>Item Name</th>
+              <th>Item #</th>
+              <th>Description</th>
+              <th># Cases</th>
+              <th># Units</th>
             </tr>
+            {data.lineItem.map((line) => (
+              <tr key={line._id}>
+                <td>{line?.item?.name ?? ''}</td>
+                <td>{line?.item?.number ?? ''}</td>
+                <td>{line?.item?.description ?? ''}</td>
+                <td>{line?.quantity_cases ?? ''}</td>
+                <td>{line?.quantity_units ?? ''}</td>
+              </tr>
             ))}
-        </table> : null}
-
+          </table>
+        ) : null}
         <p>Thank you for your order.</p>
         <p>{date.toString()}</p>
-            </div>
-        </React.Fragment>
-    )
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default PackingSlip
+export default PackingSlip;
